f666354da8d4adc213905b2327ee635b24af0148
DezkaReid <elmaildeldezkareid@gmail.com>
2018-02-21 03:14:31 -0600

    jsx and function components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hello from './samples/1-hello/Hello'
+// import Hello from './samples/1-hello/Hello'
+import HelloFunction from './samples/2-jsx-components/HelloFunction'
 
 const mainElement = document.getElementById('app')
 
-ReactDOM.render(<Hello />, mainElement)
+// ReactDOM.render(<Hello />, mainElement)
+ReactDOM.render(<HelloFunction />, mainElement)
---
+++ src/samples/2-jsx-components/HelloFunction.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HelloFuntion (props) {
+  return <h1>Hello I'm a Function Component and i'm made with JSX</h1>
+}
+
+export default HelloFuntion
