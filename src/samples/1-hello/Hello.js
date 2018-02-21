2886220d7825e804b5084a85785d12afc64b4378
DezkaReid <elmaildeldezkareid@gmail.com>
2018-02-21 02:38:08 -0600

    Hello component

---
+++ src/samples/1-hello/Hello.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class HelloComponent extends Component {
+  render () {
+    return (<h1>Hello I'm a Component and i'm made with JSX</h1>)
+  }
+}
+
+export default HelloComponent
