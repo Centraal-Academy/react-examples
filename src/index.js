954e4b9eb09cbfd14a7a25adfdb0b5e989034e20
DezkaReid <elmaildeldezkareid@gmail.com>
2018-02-21 02:36:49 -0600

    source files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './samples/1-hello/Hello'
+
+const mainElement = document.getElementById('app')
+
+ReactDOM.render(<Hello />, mainElement)
